a0b38b5d2af094eb51fdc8375d01d8d1857dba82
gonzalo <larroyagonzalo+git@gmail.com>
2026-06-14 20:59:21 +0100

    faet: implement missing functions and move `split` inductive definition

--- proof_checker/src/Split.v
+++ proof_checker/src/Split.v
@@ -1,6 +1,16 @@
 From Stdlib Require Import Reals.
+From Stdlib Require Import List.
 
-Require Import Certificate.
+Require Import Constraint.
+Require Import Util.
+
+Import ListNotations.
+
+Module Split.
+
+Inductive split :=
+| single : nat -> R -> split
+| relu : nat -> nat -> nat -> split.
 
 (*let update_bounds_from_split (lbs: real list) (ubs: real list) (split: t): *)
 (*        ((real list * real list) * (real list * real list)) =*)
@@ -16,15 +26,29 @@ Require Import Certificate.
 (*        let ubs_r = set_nth ubs aux 0. in*)
 (*        (lbs_l, ubs_l), (lbs_r, ubs_r)*)
 
-(* TODO: Implement function*)
 Definition update_bounds_from_split (lbs: list R) (ubs : list R) (split : split)
   : ((list R * list R)  * (list R * list R)) :=
-  ((nil, nil), (nil, nil)).
+  match split with
+  | single i k => ((lbs, set_nth ubs i k), (set_nth lbs i k, ubs))
+  | relu b f aux => 
+    (* left: inactive phase *)
+    let lbs_l := set_nth lbs f 0%R in
+    let ubs_l := set_nth (set_nth ubs b 0%R) f 0%R in
+    (* right: active phase *)
+    let lbs_r := set_nth (set_nth lbs b 0%R) aux 0%R in
+    let ubs_r := set_nth ubs aux 0%R in
+    ((lbs_l, ubs_l), (lbs_r, ubs_r))
+  end.
 
 (*let check_split split constraints = *)
 (*    match split with *)
 (*    | ReluSplit (b,f,aux) -> List.mem (Relu (b,f,aux)) constraints*)
 (*    | _ -> true*)
 
-Definition check_split (split : split) (constraints : list constraint) : bool :=
-  true.
+Definition check_split (split : split) (constraints : list Constraint.constraint) : bool :=
+  match split with
+  | relu b f aux  => existsb (Constraint.constraint_eqb (Constraint.relu b f aux)) constraints
+  | _ => false
+  end.
+
+End Split.
