6af48785b817b4446736344a3e718a01d66b6075
gonzalo <larroyagonzalo+git@gmail.com>
2026-06-14 21:01:28 +0100

    chore: move inductive definitions out of Certificate.v

--- proof_checker/src/Certificate.v
+++ proof_checker/src/Certificate.v
@@ -1,21 +1,15 @@
 From Stdlib Require Import Reals.
 From Stdlib Require Import List.
-Import ListNotations.
-Require Import Farkas.
 
-(* WARN: Should I move each inductive definition to a separate file? (e.g. splig -> Split.v)  *)
-Inductive split :=
-| single : nat -> R -> split
-| relu : nat -> nat -> nat -> split.
+Require Import Farkas.
+Require Import Split.
+
+Import ListNotations.
+
 
 Inductive proof_tree : Type :=
   | node : split -> proof_tree -> proof_tree -> proof_tree
   | leaf : list R -> proof_tree.
 
-(* TODO: Formalise constraints correctly*)
-Inductive constraint : Type :=
-  | node' : split -> constraint -> constraint -> constraint
-  | leaf' : list R -> constraint.
-
-(* TODO: Formalise tableau ???*)
+(* TODO: Formalise tableau ??? *)
 
