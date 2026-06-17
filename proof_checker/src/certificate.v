d27677f427752219a6ce37d4998ee39cdeb7262b
gonzalo <larroyagonzalo+git@gmail.com>
2026-06-17 21:32:47 +0100

    feat: create global vector and matrices specs

--- proof_checker/src/certificate.v
+++ proof_checker/src/certificate.v
@@ -1,14 +1,23 @@
 From Stdlib Require Import Reals.
 From Stdlib Require Import List.
 
-Require Import farkas.
+From mathcomp Require Import all_ssreflect all_algebra.
+
 Require Import split.
 
 Import ListNotations.
 
+Module CertificateSpecs.
+  Definition R : numFieldType := rat.
+  (* NOTE: Number of items in each row *)
+  Parameter (n : nat).
+  (* NOTE:  Number of  rows *)
+  Parameter (m : nat).
+End CertificateSpecs.
+
+Import CertificateSpecs.
+
 Inductive proof_tree : Type :=
   | node : Split.t -> proof_tree -> proof_tree -> proof_tree
-  | leaf : list R -> proof_tree.
-
-(* TODO: Formalise tableau ??? *)
+  | leaf : seq R -> proof_tree.
 
