56354db51692eb48a8cf1be38553d42c0a1960c6
gonzalo <larroyagonzalo+git@gmail.com>
2026-06-19 16:13:16 +0100

    feat: implement missing function in proof checker `update_bounds`

--- proof_checker/src/tightening.v
+++ proof_checker/src/tightening.v
@@ -2,6 +2,7 @@ From mathcomp Require Import all_ssreflect all_algebra.
 From Stdlib Require Import Reals.
 
 Require Import certificate_specs.
+Require Import util.
 
 Import CertificateSpecs.
 
@@ -16,6 +17,31 @@ Definition t : Type := nat * R * bound_type.
 
 Definition t_bounds : Type := 'rV[R]_n.+1.
 
+Definition is_upper (type : bound_type) : bool :=
+  match type with
+  | UPPER => true
+  | _ => false
+  end.
+
+Definition is_lower (type : bound_type) : bool :=
+  match type with
+  | LOWER => true
+  | _ => false
+  end.
+
+(*let rec update_bounds_by_tightenings (tightenings: t list) (bounds: Real.t list): Real.t list =*)
+(*   match tightenings with*)
+(*   | []                    -> bounds*)
+(*   | (var, value, _) :: tl -> let updated_bounds = set_nth bounds var value in*)
+(*                              update_bounds_by_tightenings tl updated_bounds*)
+Fixpoint update_bounds_by_tightenings (tightenings : seq t) (bounds : t_bounds) : Tightening.t_bounds :=
+  match tightenings with
+  | [::] => bounds
+  | (var, value, _) :: tl => let updated_bounds := set_nth_vector bounds var value in
+      update_bounds_by_tightenings tl updated_bounds
+  end.
+
+
 (* update the bounds according to a list of tightenings *)
 (*let update_bounds (tightenings: t list) (upper_bounds: Real.t list) (lower_bounds: Real.t list): (Real.t list * Real.t list) =*)
 (*   let upper_tightenings = List.filter is_upper tightenings in*)
@@ -23,13 +49,19 @@ Definition t_bounds : Type := 'rV[R]_n.+1.
 (*   let updated_upper = update_bounds_by_tightenings upper_tightenings upper_bounds in*)
 (*   let updated_lower = update_bounds_by_tightenings lower_tightenings lower_bounds in*)
 (*   (updated_upper, updated_lower)*)
-
 Definition update_bounds
-  (tightenings : list t)
-  (upper_bounds : list R)
-  (lower_bounds : list R)
-  : list R * list R :=
-  (nil, nil).
+  (tightenings : seq t)
+  (upper_bounds : t_bounds)
+  (lower_bounds : t_bounds)
+  : t_bounds * t_bounds :=
+
+  let upper_tightenings := filter (fun '(_, _, btype) => is_upper btype) tightenings in
+  let lower_tightenings := filter (fun '(_, _, btype) => is_lower btype) tightenings in
+
+  let updated_upper := update_bounds_by_tightenings upper_tightenings upper_bounds in
+  let updated_lower := update_bounds_by_tightenings lower_tightenings lower_bounds in
+
+  (updated_upper, updated_lower).
 
 End Tightening.
 
