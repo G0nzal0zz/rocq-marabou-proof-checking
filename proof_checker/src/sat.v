13d78a0ac4d50af6adf1e1e615b4cff8e0e3d1a1
gonzalo <larroyagonzalo+git@gmail.com>
2026-07-07 17:18:44 +0200

    feat: create sat definition

--- proof_checker/src/sat.v
+++ proof_checker/src/sat.v
@@ -17,5 +17,12 @@ Module Sat.
 (*    check_relu_constraints relu_constraints x) = false*)
 Definition unsat (tableau : (m.+2).-tuple ('rV[R]_n)) (upper_bounds lower_bounds : Tightening.t_bounds) (constraints : seq Constraint.t) (x : 'rV[R]_n) : bool :=
   (Arithmetic.is_in_kernel tableau x && Arithmetic.bounded x upper_bounds lower_bounds && Constraint.check_relu_constraints constraints x) == false.
-  
+
+(* let sat tableau upper_bounds lower_bounds relu_constraints x =
+    (is_in_kernel tableau x &&
+    bounded x upper_bounds lower_bounds &&
+    check_relu_constraints relu_constraints x) *)
+Definition sat (tableau : (m.+2).-tuple ('rV[R]_n)) (upper_bounds lower_bounds : Tightening.t_bounds) (constraints : seq Constraint.t) (x : 'rV[R]_n) : bool :=
+  Arithmetic.is_in_kernel tableau x && Arithmetic.bounded x upper_bounds lower_bounds && Constraint.check_relu_constraints constraints x.
+
 End Sat.
