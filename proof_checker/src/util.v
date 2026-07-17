5964ff563442c1cc3a9f6421f1e841c22cbeda16
gonzalo <larroyagonzalo+git@gmail.com>
2026-07-17 02:24:16 +0200

    chore: remove unused functions in `util.v`

--- proof_checker/src/util.v
+++ proof_checker/src/util.v
@@ -20,23 +20,7 @@ Fixpoint set_nth {A : Type} (l : list A) (idx : nat) (elem : A) :=
 Definition set_nth_vector {A : Type} {n : nat} (v : 'rV[A]_n) (idx : 'I_n) (elem : A) : 'rV[A]_n :=
     \matrix_(i, j) if j == idx then elem else v i j.
 
-(* NOTE: Like OCaml List.map2, but it does not fail if the lists have unequal lengths *)
-Fixpoint map2 {A B C: Type } (f : A -> B -> C) (a : list A) (b : list B) : list C :=
-  match a, b with
-  | [], _ => []
-  | _, []  => []
-  | hd :: tl, hd' :: tl' => f hd hd' :: map2 f tl tl'
-  end.
-
-Definition map_vector  (f : R -> R) (v : 'rV[R]_n.+1) : 'rV[R]_n.+1 :=
-  (* Reconstruct the row vector by applying 'f' to every element at index (i, j) *)
-  \row_(j < n.+1) f (v 0%R j).
-From mathcomp Require Import all_ssreflect all_algebra.
-
-Definition map2_vector {R : numFieldType} {n : nat} (f : R -> R -> R) (u v : 'rV[R]_n.+1) : 'rV[R]_n.+1 :=
-  (* Element-wise transformation across all columns *)
-  \row_(j < n.+1) f (u 0%R j) (v 0%R j).
-
+(* NOTE: Unused *)
 Definition vector_to_seq {n : nat} (v : 'rV[R]_n) : seq R :=
   [seq v 0%R i | i <- enum 'I_n].
 
