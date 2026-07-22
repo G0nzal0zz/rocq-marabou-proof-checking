ed563f2c99cf877081947e9fde263883cae9fd14
gonzalo <larroyagonzalo+git@gmail.com>
2026-07-22 02:55:51 +0200

    feat: rocq dependency graph builder

---
+++ proof_checker/src/graph_builder.v
@@ -0,0 +1,10 @@
+Require checker_soundness. 
+Require Import leaf_soundness.
+Require Import node_soundness.
+Require Import farkas_soundness.
+Require Import single_var_split_soundness.
+Require Import relu_split_soundness.
+Require Import split_soundness.
+Require Import system_soundness.
+
+Print FileDependGraph checker_soundness LeafSoundness NodeSoundness FarkasSoundness SingleSplitSoundness ReluSplitSoundness SplitSoundness SystemSoundness.
